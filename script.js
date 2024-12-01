0e652fe5d8bf1e5d35621f72496ef4a2a9e1a75e
Bimbika-Ganapathy <bimbikag@gmail.com>
2024-12-01 10:14:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// alert("hello world");
+// alert msg pops
+
+var a =prompt("enter pass")
+// outputs the entered no in the console
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("its blasting")
+}
+
+else{
+    console.log("not blasting")
+}
+
+document.title = "js_prac"
+// can chanege the whole doc name
+
+document.body.style.backgroundColor="red"
+// can change the bg of the page
