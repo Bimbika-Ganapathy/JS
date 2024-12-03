373fcec5500edb15bef85ccab9969fce1291dec3
Bimbika-Ganapathy <bimbikag@gmail.com>
2024-12-03 09:54:36 +0530

    variables js

---
+++ variables/script.js
@@ -0,0 +1,35 @@
+// var a=2;
+var b=5;
+var c="Hello";
+
+// console.log(a+b);
+// console.log(typeof a ,typeof b,typeof c);
+
+const i=1;
+// constant
+
+// var global scope
+// let local scope
+
+let a=9;
+{
+    let a=10;
+    console.log(a)
+    // 10
+}
+console.log(a)
+// 11
+
+
+var b=9;
+{
+    var b=10;
+    // console.log(b) 10
+    // updates globally the value of b 
+}
+console.log(b)
+// 10
+ 
+let y = undefined ;
+let x= null ;
+let z= Number ;
