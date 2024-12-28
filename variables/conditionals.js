d1cfaa57325ec06083b1c3e18dd2a3d70ae8408b
Bimbika-Ganapathy <bimbikag@gmail.com>
2024-12-28 23:28:08 +0530

    conditionals

---
+++ variables/conditionals.js
@@ -0,0 +1,39 @@
+let age = 18;
+let no=0;
+
+
+if(age==18){
+    console.log("Yes adult");
+}
+
+if(no!=1){
+    console.log("not 1 ")
+}
+
+ "3"==3
+true
+> "3"==="3"  //to check if value n type both are equal
+true
+> "3"===3
+false
+
+"3"!=="3"   //checks for not equal to type and value 
+false
+
+"3"!==3
+true
+
+
+> "3"!=="2"
+true
+
+
+> !true
+false
+> !false
+true
+>
+ !0
+true
+> !1
+false
